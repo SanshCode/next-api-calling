b346942abd5b862748b3a99eed920f50fb9fad85
SanshCode <131510989+SanshCode@users.noreply.github.com>
2024-01-31 23:45:59 +0530

    "using json placeholder api by axios"

---
+++ src/app/[id]/page.js
@@ -0,0 +1,27 @@
+"use client"
+
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const page =  ({params}) => {
+  const { id } = params;
+  const [users, setUsers] = useState("")
+  const getUsers = async () => {
+  const { data } = await axios.get('https://jsonplaceholder.typicode.com/users/'+id)
+    setUsers(data)
+    
+  }
+  useEffect(() => {
+    getUsers()
+  }, [])
+  
+  return (
+    <>
+    {JSON.stringify(users)}
+    
+    </>
+    
+  )
+}
+
+export default page
